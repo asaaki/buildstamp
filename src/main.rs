85a8f8095d37a10f7bbfcd322b5625699d888027
Christoph Grabo <asaaki@mannaz.cc>
2024-01-01 14:17:05 +0100

    Add MC style stamping (24w01a) and lowercasing

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,45 @@ use formatter::*;
 #[command(author, version, about, long_about)]
 struct Args {
     /// Which format to use for the buildstamp
-    #[arg(default_value_t = Format::Weekly,
+    ///
+    #[arg(
+        default_value_t = Format::Weekly,
         value_parser = PossibleValuesParser::new(Format::VALUES).map(|s| s.parse::<Format>().unwrap()),
+        ignore_case(true)
     )]
     format: Format,
+
+    /// Print all letters lowercased
+    #[arg(short, long, default_value_t = false)]
+    lowercase: bool,
+
+    /// Specify the Minecraft build iteration (A, B, C, ...)
+    ///
+    /// Works only with the "weekly" format and appends the content of this option;
+    /// always uppercase unless `--lowercase` is used.
+    ///
+    /// Note: You can also use it to put arbitrary text into the buildstamp. ;-)
+    #[arg(
+        short,
+        long,
+        env,
+        default_value_t = String::new(),
+        default_missing_value = "A",
+        num_args(0..=1),
+        ignore_case(true)
+    )]
+    minecraft: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let stamp = time::OffsetDateTime::now_utc().format(args.format.formatter())?;
-
+    let mut stamp = time::OffsetDateTime::now_utc().format(args.format.formatter())?;
+    if args.format == Format::Weekly {
+        stamp.push_str(&args.minecraft.to_ascii_uppercase())
+    }
+    if args.lowercase {
+        stamp = stamp.to_ascii_lowercase();
+    }
     print!("{stamp}");
     Ok(())
 }
