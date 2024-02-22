bea0f1b4b89267daad5bf9cd85e184314168403d
Christoph Grabo <asaaki@mannaz.cc>
2024-02-22 19:11:18 +0100

    Turn it into a lib+bin crate

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+mod formatter;
+pub use formatter::*;
+
+/// Holds information about how to output the buildstamp
+pub struct Buildstamp {
+    timestamp: time::OffsetDateTime,
+    format: Format,
+    newline: bool,
+    lowercase: bool,
+    minecraft: Option<String>,
+}
+
+impl Default for Buildstamp {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl std::fmt::Display for Buildstamp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut stamp = self.timestamp.format(self.format.formatter()).unwrap();
+
+        if self.format == Format::Weekly {
+            if let Some(minecraft) = &self.minecraft {
+                stamp.push_str(&minecraft.to_ascii_uppercase());
+            }
+        }
+        if self.lowercase {
+            stamp = stamp.to_ascii_lowercase();
+        }
+        if self.newline {
+            stamp.push('\n');
+        }
+
+        write!(f, "{stamp}")
+    }
+}
+
+impl Buildstamp {
+    /// Create a new buildstamp with the default format (weekly);
+    /// no newline, no lowercase, and no minecraft suffix
+    pub fn new() -> Self {
+        Self::new_with_format(Format::Weekly)
+    }
+
+    /// Create a new buildstamp with the given format;
+    /// no newline, no lowercase, and no minecraft suffix
+    #[inline]
+    pub fn new_with_format(format: Format) -> Self {
+        Self::new_from_args(format, false, false, None)
+    }
+
+    /// Create a new buildstamp with the given arguments
+    #[inline]
+    pub fn new_from_args(
+        format: Format,
+        newline: bool,
+        lowercase: bool,
+        minecraft: Option<String>,
+    ) -> Self {
+        Self {
+            timestamp: time::OffsetDateTime::now_utc(),
+            format,
+            newline,
+            lowercase,
+            minecraft,
+        }
+    }
+
+    /// Print the buildstamp to stdout
+    pub fn print(&self) {
+        print!("{self}");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
+use buildstamp_lib::{Buildstamp, Format};
 use clap::builder::{PossibleValuesParser, TypedValueParser};
 use clap::Parser;
-
-mod formatter;
-use formatter::*;
 
 /// Returns a buildstamp, like `23W42.12345`
 ///
@@ -30,33 +28,25 @@ struct Args {
     /// Specify the Minecraft build iteration (A, B, C, ...)
     ///
     /// Works only with the "weekly" format and appends the content of this option;
-    /// always uppercase unless `--lowercase` is used.
+    /// always uppercase unless `--lowercase` is used. Will use "A" if no value is given.
     ///
     /// Note: You can also use it to put arbitrary text into the buildstamp. ;-)
     #[arg(
         short,
         long,
         env,
-        default_value_t = String::new(),
+        // default_value_t = None,
         default_missing_value = "A",
         num_args(0..=1),
         ignore_case(true)
     )]
-    minecraft: String,
+    minecraft: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let mut stamp = time::OffsetDateTime::now_utc().format(args.format.formatter())?;
-    if args.format == Format::Weekly {
-        stamp.push_str(&args.minecraft.to_ascii_uppercase())
-    }
-    if args.lowercase {
-        stamp = stamp.to_ascii_lowercase();
-    }
-    if args.newline {
-        stamp.push('\n');
-    }
-    print!("{stamp}");
+    let stamp =
+        Buildstamp::new_from_args(args.format, args.newline, args.lowercase, args.minecraft);
+    stamp.print();
     Ok(())
 }
